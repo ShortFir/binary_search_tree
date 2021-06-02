2c0b7a2e962f1a5e42705efa609a636f10582572
ShortFir <shortfir@protonmail.com>
2021-06-02 16:57:52 +1000

    Add assignment methods to tree.rb.

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require_relative 'tree'
+
 # Do not use duplicate values because they make it more complicated
 # and result in trees that are much harder to balance. Therefore,
 # be sure to always remove duplicate values or check for an existing
 # value before inserting.
+
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+puts tree
+print tree.root, "\n"
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,6 +1,46 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Tree
 class Tree
-  def build_tree; end
+  # As a bonus, try including the Comparable module and compare nodes using
+  # their data attribute.
+  include Comparable
+  attr_reader :root
+
+  def initialize(array)
+    @root = build_tree(array.sort.uniq)
+  end
+
+  # Algorithm
+  # 1. Initialize start=0, end=length of array-1
+  # 2. mid=(start+end/2)
+  # 3. Create a tree node with mid as root(lets call it A)
+  # 4. Recursively do following steps, 5 & 6
+  # 5. Calculate mid of left subarray and make it root of left subtree of A
+  # 6. Calculate mid of right subarray and make it root of right subtree of A
+  def build_tree(array)
+    array
+  end
+
+  def insert(value); end
+
+  def delete(value); end
+
+  def find(value); end
+
+  def level_order; end
+
+  def inorder; end
+
+  def preorder; end
+
+  def postorder; end
+
+  def height(node); end
+
+  def balanced?; end
+
+  def rebalance; end
 end
